d2ab792c3afdc9601b81eb67b51fea6f6b80ff45
Swartzpanzer <ravenz1995@yandex.ru>
2021-12-28 17:40:24 +0300

    upd layout

--- src/app.js
+++ src/app.js
@@ -6,12 +6,7 @@ import './assets/js/index.js';
 // sass
 import './assets/sass/index.sass';
 
-// vue
-import './vue/vue-apps';
+// // vue
+// import './vue/vue-apps';
 
-// sprite-svg
-function requireAll(r) {
-  r.keys().forEach(r);
-}
 
-requireAll(require.context('./assets/img/svg-templates/', true, /\.svg$/));
